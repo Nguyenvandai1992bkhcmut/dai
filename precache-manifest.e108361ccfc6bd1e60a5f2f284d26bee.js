454222ee4ef5c0ce8411e91a4209c3974f6f7cb3
vandai.nguyen <vandai.nguyen@ved.com.vn>
2019-07-26 00:01:38 +0700

    Updates

---
+++ precache-manifest.e108361ccfc6bd1e60a5f2f284d26bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726617e0d4b9305f1346c1d4ede6433f",
+    "url": "/dai/index.html"
+  },
+  {
+    "revision": "43fadec4110afed32308",
+    "url": "/dai/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/dai/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "43fadec4110afed32308",
+    "url": "/dai/static/js/main.1e072e19.chunk.js"
+  },
+  {
+    "revision": "89413c219a560190381c",
+    "url": "/dai/static/js/runtime~main.36b82c86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dai/static/media/logo.5d5d9eef.svg"
+  }
+]);
